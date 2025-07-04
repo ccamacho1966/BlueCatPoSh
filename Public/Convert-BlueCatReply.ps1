df12d1be3ed5010ab752a59adb887b2eca78ea5a
Christopher <81055999+ccamacho1966@users.noreply.github.com>
2025-07-04 17:59:06 -0400

    improve logic, preserve bluecat info
    Redesign logic flow, better preserve all data being retrieved from BlueCat in objects being passed back to caller

--- Public/Convert-BlueCatReply.ps1
+++ Public/Convert-BlueCatReply.ps1
@@ -1,83 +1,69 @@
 ﻿function Convert-BlueCatReply {
     [cmdletbinding()]
     param(
+        [Parameter(ValueFromPipeline,Mandatory)]
+        [PSCustomObject] $RawObject,
+
         [Parameter()]
         [Alias('Connection','Session')]
-        [BlueCat] $BlueCatSession = $Script:BlueCatSession,
-
-        [Parameter(ValueFromPipeline,Mandatory)]
-        [psobject] $RawObject,
-
-        [Alias('Config')]
-        [psobject] $Configuration,
-
-        [psobject] $View
+        [BlueCat] $BlueCatSession = $Script:BlueCatSession
     )
 
     begin { Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState }
 
     process {
-        if ($Configuration) { $configObj = $Configuration}
-        else { $configObj = ($BlueCatSession | Get-BlueCatConfig) }
+        # All BlueCat objects have an ID, name, and object type
+        $newObj = New-Object -TypeName PSCustomObject
+        $newObj | Add-Member -MemberType NoteProperty -Name 'id'   -Value $RawObject.id
+        $newObj | Add-Member -MemberType NoteProperty -Name 'name' -Value $RawObject.name
+        $newObj | Add-Member -MemberType NoteProperty -Name 'type' -Value $RawObject.type
 
-        if ($View) { $viewObj = $View }
-        else { $viewObj = ($BlueCatSession | Get-BlueCatView) }
-
-        $newObj = New-Object -TypeName psobject
-        if ($RawObject.type -eq 'Configuration') {
-            $newObj | Add-Member -MemberType NoteProperty -Name 'id'   -Value $RawObject.id
-            $newObj | Add-Member -MemberType NoteProperty -Name 'name' -Value $RawObject.name
-            $newObj | Add-Member -MemberType NoteProperty -Name 'type' -Value $RawObject.type
-        } elseif ($RawObject.type -eq 'View') {
-            $newObj | Add-Member -MemberType NoteProperty -Name 'id' -Value $RawObject.id
-            $newObj | Add-Member -MemberType NoteProperty -Name 'name' -Value $RawObject.name
-            $newObj | Add-Member -MemberType NoteProperty -Name 'type' -Value $RawObject.type
-            $configObj = Get-BlueCatParent -Connection $BlueCatSession -id $RawObject.id
-            $newObj | Add-Member -MemberType NoteProperty -Name 'config' -Value $configObj
-        } else {
-            foreach ($bit in $RawObject.PsObject.Properties) {
-                if ($bit.Name -eq 'properties') {
-                    if ($null -eq $bit.Value) {
-                        $newPropObj = $null
-                    } else {
-                        $newPropObj = Convert-BlueCatPropertyString -PropertyString $bit.Value
-                        if ($newPropObj.addresses) {
-                            $addrObj = ($newPropObj.addresses -split ',' | Where-Object -FilterScript { $_ })
-                            $newPropObj | Add-Member -MemberType NoteProperty -Name 'address' -Value $addrObj
-                        }
-                    }
-                    if ($RawObject.type -match '^IP4[BN].*') {
-                        if ($newPropObj.CIDR) {
-                            $specs = [PSCustomObject]@{'type'='CIDR'; 'spec'=$($newPropObj.CIDR)}
-                        } else {
-                            $specs = [PSCustomObject]@{'type'='Range'; 'spec'="$($newPropObj.start)-$($newPropObj.end)"}
-                        }
-                        $newPropObj | Add-Member -MemberType NoteProperty -Name ($RawObject.type.toLower()) -Value $specs
-                    }
-                    $newObj | Add-Member -MemberType NoteProperty -Name 'property' -Value $newPropObj
-                }
-                $newObj | Add-Member -MemberType NoteProperty -Name $bit.Name -Value $bit.Value
+        # Create a custom sub-object from the BlueCat properties string
+        if ($RawObject.properties) {
+            $newPropObj = $RawObject.properties | Convert-BlueCatPropertyString
+            if ($newPropObj.addresses) {
+                $addrObj = ($newPropObj.addresses -split ',' | Where-Object -FilterScript { $_ })
+                $newPropObj | Add-Member -MemberType NoteProperty -Name 'address' -Value $addrObj
             }
+            if ($RawObject.type -match '^IP4[BN].*') {
+                # Create a 'specs' object member for IP4 Blocks and Networks
+                if ($newPropObj.CIDR) {
+                    $specs = [PSCustomObject]@{'type'='CIDR'; 'spec'=$($newPropObj.CIDR)}
+                } else {
+                    $specs = [PSCustomObject]@{'type'='Range'; 'spec'="$($newPropObj.start)-$($newPropObj.end)"}
+                }
+                $newPropObj | Add-Member -MemberType NoteProperty -Name ($RawObject.type.toLower()) -Value $specs
+            }
+            # Add both the original BlueCat string and the newly created object to the custom object
+            $newObj | Add-Member -MemberType NoteProperty -Name 'property'   -Value $newPropObj
+            $newObj | Add-Member -MemberType NoteProperty -Name 'properties' -Value $RawObject.properties
+        }
 
-            # Create a 'shortName' field and replace the default name field with the absolute name
+        if ($RawObject.type -eq 'View') {
+            # Add a Config reference to views and that is all
+            $configObj = Get-BlueCatParent -Connection $BlueCatSession -id $RawObject.id
+            $newObj    | Add-Member -MemberType NoteProperty -Name 'config' -Value $configObj
+        } elseif ($RawObject.type -notin ('Configuration','PublishedServerInterface')) {
+            # Configurations and Server Interfaces do not get config or view references
+
+            # Create a 'shortName' for DNS records - Replace the default name with the absolute name
             if (($newObj.type -eq 'Zone') -or ($newObj.type -match '.*Record$')) {
                 $newObj | Add-Member -MemberType NoteProperty -Name 'shortName' -Value $newObj.name
                 if ($newObj.property.absoluteName) { $newObj.name = $newObj.property.absoluteName }
             }
 
-            Switch ($newObj.type) {
-                {$_ -eq 'PublishedServerInterface'} {
-                } # Do not include config or view references
+            # Conditionally add config and view references to objects
+            if (($newObj.type -eq 'Server') -or ($newObj.type -match '^IP4[BNA].*')) {
+                # Only include config reference
+                $configObj = Trace-BlueCatConfigFor -id $newObj.id -Connection $BlueCatSession
+            } else {
+                # Include both a config and view reference
+                $viewObj   = Trace-BlueCatViewFor -id $newObj.id -Connection $BlueCatSession
+                $configObj = Get-BlueCatParent -Connection $BlueCatSession -id $viewObj.id
+            }
 
-                {($_ -eq 'Server') -or ($_ -match '^IP4[BNA].*')} {
-                    $newObj | Add-Member -MemberType NoteProperty -Name config -Value $($configObj)
-                } # Only include config reference
-
-                default {
-                    $newObj | Add-Member -MemberType NoteProperty -Name config -Value $($configObj)
-                    $newObj | Add-Member -MemberType NoteProperty -Name view   -Value $($viewObj)
-                } # By default include both a config and view reference
-            } # conditionally add config and view references to objects
+            if ($configObj) { $newObj | Add-Member -MemberType NoteProperty -Name config -Value $($configObj) }
+            if ($viewObj)   { $newObj | Add-Member -MemberType NoteProperty -Name view   -Value $($viewObj)   }
         }
 
         # Return the converted reply
