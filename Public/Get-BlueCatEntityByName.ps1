3649ae6d3d647f24ad4a3b93ba1ac59713700eae
Christopher <81055999+ccamacho1966@users.noreply.github.com>
2025-07-04 18:02:12 -0400

    remove redundant code

--- Public/Get-BlueCatEntityByName.ps1
+++ Public/Get-BlueCatEntityByName.ps1
@@ -1,43 +1,45 @@
 ﻿function Get-BlueCatEntityByName {
     [cmdletbinding()]
     param(
-        [Parameter()]
-        [Alias('Connection','Session')]
-        [BlueCat] $BlueCatSession = $Script:BlueCatSession,
-
-        [parameter(Mandatory)]
+        [Parameter(Mandatory)]
+        [Alias('EntityName')]
         [string] $Name,
 
-        [parameter(Mandatory)]
+        [Parameter(Mandatory)]
         [string] $EntityType,
 
         [Parameter()]
-        [int] $ParentID
+        [int] $ParentID,
+
+        [Parameter()]
+        [Alias('Connection','Session')]
+        [BlueCat] $BlueCatSession = $Script:BlueCatSession
     )
 
     begin { Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState }
 
     process {
-        if (-not $ParentID) {
+        $verboseOutput = "Get-BlueCatEntityByName: Name='$($Name)', EntityType='$($EntityType)'"
+        if ($ParentID) { $verboseOutput += ", ParentID='$($ParentID)'" }
+        Write-Verbose $verboseOutput
+
+        if ($EntityType -eq 'Configuration') {
+            if ($ParentID) {
+                Write-Warning "Ignoring ParentID for EntityType=Configuration lookup..."
+            }
+            $ParentID=0
+        } elseif (-not $ParentID) {
             $BlueCatSession | Confirm-Settings -Config
             $ParentID = $BlueCatSession.idConfig
         }
 
         $Query = "getEntityByName?parentId=$($ParentID)&name=$($Name)&type=$($EntityType)"
         $result = Invoke-BlueCatApi -BlueCatSession $BlueCatSession -Method Get -Request $Query
-        if ($result.id -eq 0) {
+        if (-not $result.id) {
             Throw "$($EntityType) $($Name) not found: $($result)"
         }
-        Write-Verbose "Selected $($result.type) #$($result.id) as $($result.name)"
+        Write-Verbose "Get-BlueCatEntityByName: Selected $($result.type) #$($result.id) as $($result.name)"
 
-        $objConfig = $objView = $null
-        if ($result.type -ne 'Configuration') {
-            $objConfig = Trace-BlueCatConfigFor -id $result.id -Connection $BlueCatSession
-            if (($result.type -ne 'View') -and ($result.type -notmatch '^IP4[BNA].*')) {
-                $objView = Trace-BlueCatViewFor -id $result.id -Connection $BlueCatSession
-            }
-        }
-
-        $result | Convert-BlueCatReply -BlueCatSession $BlueCatSession -Configuration $objConfig -View $objView
+        $result | Convert-BlueCatReply -BlueCatSession $BlueCatSession
     }
 }
