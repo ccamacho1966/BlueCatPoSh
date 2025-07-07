25ea2ca3cb0e779573adbea6bb6e8236bdc14baa
Christopher <81055999+ccamacho1966@users.noreply.github.com>
2025-07-07 17:28:45 -0400

    new function: Get-BlueCatEntities

---
+++ Public/Get-BlueCatEntities.ps1
@@ -0,0 +1,44 @@
+function Get-BlueCatEntities {
+    [cmdletbinding()]
+
+    param(
+        [Parameter(Mandatory)]
+        [Alias('ParentID')]
+        [int] $Parent,
+
+        [Parameter(Mandatory)]
+        [string] $EntityType,
+
+        [int] $Start = 0,
+
+        [int] $Count = 10,
+
+        [Parameter()]
+        [Alias('Connection','Session')]
+        [BlueCat] $BlueCatSession = $Script:BlueCatSession
+    )
+
+    begin { Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState }
+
+    process {
+        $thisFN = (Get-PSCallStack)[0].Command
+
+        Write-Verbose "$($thisFN): EntityType='$($EntityType)', Parent='$($Parent)', Start=$($Start), Count=$($Count)"
+
+        if ($EntityType -eq 'Configuration') {
+            if ($Parent) {
+                Write-Warning "Ignoring Parent ID for EntityType=Configuration lookup..."
+            }
+            $Parent=0
+        }
+
+        $Query = "getEntities?parentId=$($Parent)&type=$($EntityType)&start=$($Start)&count=$($Count)"
+        [PSCustomObject[]] $BlueCatReply = Invoke-BlueCatApi -Method Get -Request $Query -BlueCatSession $BlueCatSession
+
+        Write-Verbose "$($thisFN): Retrieved $($BlueCatReply.Count) records"
+
+        if ($BlueCatReply) {
+            $BlueCatReply | Convert-BlueCatReply -BlueCatSession $BlueCatSession
+        }
+    }
+}
