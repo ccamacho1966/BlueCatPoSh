e42da01659e1721381ef481f78227dc81958a212
Christopher <81055999+ccamacho1966@users.noreply.github.com>
2025-07-15 17:38:25 -0400

    Improve handling of default session
    Implement better logic to detect and clear the default BlueCat session if it is disconnected by this function.
    
    Also add comment-based help.

--- Public/Disconnect-BlueCat.ps1
+++ Public/Disconnect-BlueCat.ps1
@@ -1,4 +1,36 @@
 function Disconnect-BlueCat {
+<#
+.SYNOPSIS
+    Disconnect an active BlueCatPoSh session.
+.DESCRIPTION
+    The Disconnect-BlueCat cmdlet ends an active BlueCatPoSh session. This invokes the logout API endpoint
+    to cleanly terminate the session to the IPAM appliance. The session is removed from the list of active
+    sessions. If you disconnect the module default session the $BlueCatSession variable will be set to $null.
+.PARAMETER BlueCatSession
+    A BlueCat object representing the session to be disconnected.
+.EXAMPLE
+    PS> Disconnect-BlueCat
+
+    Disconnects the current default active BlueCat session listed in the $BlueCatSession variable.
+    The session will be removed from the list of all active sessions.
+    The default session will be set to $null.
+    .EXAMPLE
+    PS> Disconnect-BlueCat -BlueCatSession $MyBlueCatSession
+
+    Disconnects the active BlueCat session listed in $MyBlueCatSession.
+    The session will be removed from the list of all active sessions.
+    If this is the default session then $BlueCatSession will be set to $null.
+.EXAMPLE
+    PS> $MyBlueCatSession | Disconnect-BlueCat
+
+    Disconnects the active BlueCat session piped into the cmdlet.
+    The session will be removed from the list of all active sessions.
+    If this is the default session then $BlueCatSession will be set to $null.
+.INPUTS
+    [BlueCat] can be piped to the Disconnect-BlueCat cmdlet.
+.OUTPUTS
+    None.
+#>
     [CmdletBinding()]
 
     param(
@@ -15,15 +47,20 @@ function Disconnect-BlueCat {
         if (-not $BlueCatSession) {
             # If we're using the default then erase the default
             $BlueCatSession = $Script:BlueCatSession
-            $Script:BlueCatSession = $null
         }
 
         if ($BlueCatSession) {
-            # Remove this session for the internal list of active BlueCat sessions
+            if ($BlueCatSession.idTag -eq $Script:BlueCatSession.idTag) {
+                # If this is the default session then set the module reference to $null
+                Write-Verbose "$($thisFN): Default BlueCatSession has been reset to NULL"
+                $Script:BlueCatSession = $null
+            }
+
+            # Remove this session from the internal list of active BlueCat sessions
             [BlueCat[]] $Script:BlueCatAllSessions = $Script:BlueCatAllSessions | Where-Object -Property 'idTag' -NE -Value $BlueCatSession.idTag
 
             # Invoke the API logout endpoint, but do not trigger an error if it fails
-            $Result = Invoke-BlueCatApi -BlueCatSession $BlueCatSession -Method Get -Request 'logout'
+            $Result = Invoke-BlueCatApi -Method Get -Request 'logout' -BlueCatSession $BlueCatSession -ErrorAction SilentlyContinue
             Write-Verbose "$($thisFN): $($Result)"
         } else {
             Write-Verbose "$($thisFN): No active BlueCat session to disconnect"
