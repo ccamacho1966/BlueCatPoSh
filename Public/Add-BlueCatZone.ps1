99145dc4f97536e3a1eccc25dd8efd77cab37e2c
Christopher <81055999+ccamacho1966@users.noreply.github.com>
2025-07-19 18:47:01 -0400

    add help, improve messages
    Add comment-based help.
    
    Improve quality of verbose messages.
    
    Variable naming consistency changes.

--- Public/Add-BlueCatZone.ps1
+++ Public/Add-BlueCatZone.ps1
@@ -1,5 +1,31 @@
 ﻿function Add-BlueCatZone
 {
+<#
+.SYNOPSIS
+    Create a new DNS zone.
+.DESCRIPTION
+    The Add-BlueCatZone cmdlet will create a new DNS zone under an existing DNS View.
+.PARAMETER Name
+    A string value representing the FQDN of the new DNS Zone.
+.PARAMETER NotDeployable
+    A switch that indicates the new DNS Zone should NOT be deployed.
+
+    By default, newly created zones are configured to be deployable. This switch allows you to override that default.
+.PARAMETER Property
+    A PSCustomObject representing the DNS Zone object properties.
+.PARAMETER ViewID
+    An integer value representing the entity ID of the desired view.
+.PARAMETER View
+    A PSCustomObject representing the desired view.
+.PARAMETER BlueCatSession
+    A BlueCat object representing the session to be used for this object creation.
+.PARAMETER PassThru
+    A switch that causes a PSCustomObject representing the new DNS Zone to be returned.
+.EXAMPLE
+.EXAMPLE
+.INPUTS
+.OUTPUTS
+#>
     [CmdletBinding(DefaultParameterSetName='ViewID')]
 
     param(
@@ -11,10 +37,11 @@
 
         [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [PSCustomObject] $Properties,
+        [Alias('Properties')]
+        [PSCustomObject] $Property,
 
         [Parameter(ParameterSetName='ViewID')]
-        [ValidateNotNullOrEmpty()]
+        [ValidateRange(1, [int]::MaxValue)]
         [int] $ViewID,
 
         [Parameter(ParameterSetName='ViewObj',Mandatory)]
@@ -40,15 +67,19 @@
         }
 
         if ($ViewID) {
-            $LookupParms.ViewID = $ViewID
+            $View             = Get-BlueCatView -ViewID $ViewID -BlueCatSession $BlueCatSession
+            $LookupParms.View = $View
         } elseif ($View)   {
-            $LookupParms.View   = $View
-            $ViewID             = $View.ID
+            $LookupParms.View = $View
+            $ViewID           = $View.ID
         } else {
             $BlueCatSession | Confirm-Settings -View
-            $ViewID             = $BlueCatSession.idView
-            $LookupParms.ViewID = $ViewID
+            $View             = Get-BlueCatView -ViewID $BlueCatSession.idView -BlueCatSession $BlueCatSession
+            $ViewID           = $View.ID
+            $LookupParms.View = $View
         }
+
+        Write-Verbose "$($thisFN): Create new zone $($Name) in View $($View.Name) under Configuration $($View.Config.Name)"
 
         try {
             $ZoneCheck = Get-BlueCatZone @LookupParms
@@ -59,15 +90,30 @@
             throw "Zone $($ZoneCheck.name) already exists"
         }
 
-        if ($NotDeployable) {
-            $propString='deployable=false|'
+        if ($Property) {
+            if ($Property.PSObject.Properties.Name -contains 'deployable') {
+                # The 'deployable' member already exists in supplied Property object...
+                if ($Property.deployable -and $NotDeployable) {
+                    # Property object says deployable, but -NotDeployable switch was set
+                    Write-Warning "$($thisFN): Overwriting Property.deployable (-NotDeployable was requested)"
+                    $Property.deployable = $false
+                }
+            } else {
+                # The 'deployable' member doesn't exist so create a new member
+                $Property | Add-Member -MemberType NoteProperty -Name 'deployable' -Value ([bool] (-not $NotDeployable))
+            }
         } else {
-            $propString='deployable=true|'
+            # No property object was supplied by the caller
+            $Property = [PSCustomObject] @{
+                deployable = ([bool] (-not $NotDeployable))
+            }
         }
+
+        $PropertyString = $Property | Convert-BlueCatPropertyObject
 
         $CreateZone = @{
             Method         = 'Post'
-            Request        = "addZone?parentId=$($ViewID)&absoluteName=$($NewZone)&properties=$($propString)"
+            Request        = "addZone?parentId=$($ViewID)&absoluteName=$($NewZone)&properties=$($PropertyString)"
             BlueCatSession = $BlueCatSession
         }
 
@@ -76,7 +122,7 @@
             throw "Host creation failed for $($NewHost) - $($BlueCatReply)"
         }
 
-        Write-Verbose "$($thisFN): Created #$($BlueCatReply) as '$($NewZone)'"
+        Write-Verbose "$($thisFN): Created new zone $($NewZone) in View $($View.Name) under Configuration $($View.Config.Name) (ID:$($BlueCatReply))"
 
         if ($PassThru) {
             Get-BlueCatEntityById -ID $BlueCatReply -BlueCatSession $BlueCatSession
