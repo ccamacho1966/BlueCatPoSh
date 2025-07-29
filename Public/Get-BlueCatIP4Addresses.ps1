1ef1835bb93ea65f66c9e0e48a7187791ba8fd2f
Christopher <81055999+ccamacho1966@users.noreply.github.com>
2025-07-29 14:39:48 -0400

    comment based help

--- Public/Get-BlueCatIP4Addresses.ps1
+++ Public/Get-BlueCatIP4Addresses.ps1
@@ -1,12 +1,49 @@
 ﻿function Get-BlueCatIP4Addresses {    # no corresponding API call
+<#
+.SYNOPSIS
+    Retrieve IP4 Addresses
+.DESCRIPTION
+    The Get-BlueCatIP4Addresses cmdlet allows the retrieval of IP4 addresses associated with a specified IP4 network.
+.PARAMETER Network
+    A PSCustomObject that represents the IP4 network to be searched.
+.PARAMETER NetworkID
+    An integer value that represents the entity ID of the IP4 network to be searched.
+.PARAMETER Start
+    An integer value defining the index to start returning results from. The default start value is 0.
+.PARAMETER Count
+    An integer value defining how many results to return for each query. The default count value is 10.
+.PARAMETER BlueCatSession
+    A BlueCat object representing the session to be used for this object lookup.
+.EXAMPLE
+    PS> Get-BlueCatIP4Addresses -Network $MyNetwork
+
+    Returns one or more PSCustomObjects representing IP4 addresses under the $MyNetwork IP4 network object.
+    A null value is returned if the search finds no IP4 addresses.
+    BlueCatSession will default to the current default session.
+    Start will default to 0. Count will default to 10.
+.EXAMPLE
+    PS> Get-BlueCatEntities -NetworkID 117273 -Start 100 -Count 100 -BlueCatSession $Session7
+
+    Returns one or more PSCustomObjects representing IP4 addresses under the supplied entity ID, which should be a IP4 network.
+    A null value is returned if the search finds no IP4 addresses (or there are less than 100 IP4 addresses in total).
+    BlueCatSession $Session7 will be used for this search.
+    Since Start is set to 100, the first 100 IP4 addresses (index 0-99) will be skipped.
+    Since Count is set to 100, objects 100-199 will be returned.
+.INPUTS
+    None
+.OUTPUTS
+    One or more PSCustomObjects representing IP4 addresses.
+#>
     [CmdletBinding()]
 
     param(
         [Parameter(Mandatory,ParameterSetName='byObj')]
+        [Alias('Parent')]
         [PSCustomObject] $Network,
 
         [Parameter(Mandatory,ParameterSetName='byID')]
         [ValidateRange(1, [int]::MaxValue)]
+        [Alias('ParentID')]
         [int] $NetworkID,
 
         [Parameter()]
