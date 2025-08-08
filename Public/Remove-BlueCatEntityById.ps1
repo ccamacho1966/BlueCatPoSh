2fbbf951c658ef341e191c991f38bdc46ab7233b
Christopher <81055999+ccamacho1966@users.noreply.github.com>
2025-08-08 12:11:48 -0400

    Create Remove-BlueCatEntityById.ps1

---
+++ Public/Remove-BlueCatEntityById.ps1
@@ -0,0 +1,61 @@
+function Remove-BlueCatEntityById {
+    [CmdletBinding()]
+
+    param(
+        [parameter(Mandatory)]
+        [Alias('EntityID')]
+        [ValidateRange(1, [int]::MaxValue)]
+        [int] $ID,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [hashtable] $Options,
+
+        [Parameter()]
+        [Alias('Connection','Session')]
+        [BlueCat] $BlueCatSession = $Script:BlueCatSession
+    )
+
+    begin {
+        Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+        if (-not $BlueCatSession) { throw 'No active BlueCatSession found' }
+    }
+
+    process {
+        $thisFN = (Get-PSCallStack)[0].Command
+
+        Write-Verbose "$($thisFN): ID=$($ID)"
+
+        $DeleteObject = @{
+            Method         = 'Delete'
+            BlueCatSession = $BlueCatSession
+        }
+
+        if ($Options) {
+            # Handling options means using the deleteWithOptions API call
+            $DeleteObject.Request = "deleteWithOptions?objectId=$($ID)"
+
+            #Process Options hashtable
+            foreach ($item in $Options) {
+                $thisKey = $item.key
+
+                if ($item.Value.GetType() -eq 'Boolean') {
+                    $thisValue = $item.Value.toString().toLower()
+                } else {
+                    $thisValue = $item.Value
+                }
+
+                $DeleteObject.Request += "&$($thisKey)=$($thisValue)"
+            }
+        } else {
+            # Default delete API call with no options
+            $DeleteObject.Request = "delete?objectId=$($ID)"
+        }
+
+        $BlueCatReply = Invoke-BlueCatApi @DeleteObject
+
+        if ($BlueCatReply) {
+            Write-Warning "$($thisFN): Unexpected reply: $($BlueCatReply)"
+        }
+    }
+}
