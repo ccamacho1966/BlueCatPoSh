60e0d476f7b8553358f2158d3f6aedf2c46e4625
Christopher <81055999+ccamacho1966@users.noreply.github.com>
2025-08-08 12:42:11 -0400

    Create Remove-BlueCatAlias.ps1

---
+++ Public/Remove-BlueCatAlias.ps1
@@ -0,0 +1,99 @@
+function Remove-BlueCatAlias {
+    [CmdletBinding(DefaultParameterSetName='byID')]
+
+    param(
+        [Parameter(ParameterSetName='byID',Mandatory)]
+        [ValidateRange(1, [int]::MaxValue)]
+        [Alias('EntityID')]
+        [int] $ID,
+
+        [Parameter(ParameterSetName='byObj',Mandatory,ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Entity')]
+        [PSCustomObject] $Object,
+
+        [Parameter(ParameterSetName='byNameViewID',Mandatory)]
+        [Parameter(ParameterSetName='byNameViewObj',Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('AliasName')]
+        [string] $Name,
+
+        [Parameter(ParameterSetName='byNameViewID',Mandatory)]
+        [ValidateRange(1, [int]::MaxValue)]
+        [int] $ViewID,
+
+        [Parameter(ParameterSetName='byNameViewObj',Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [PSCustomObject] $View,
+
+        [Parameter()]
+        [Alias('Connection','Session')]
+        [BlueCat] $BlueCatSession = $Script:BlueCatSession
+    )
+
+    begin {
+        Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+        if (-not $BlueCatSession) { throw 'No active BlueCatSession found' }
+    }
+
+    process {
+        $thisFN = (Get-PSCallStack)[0].Command
+
+        if ($ID) {
+            # Convert the Entity ID into an Entity Object to use the object logic
+            $Object = Get-BlueCatEntityById -ID $ID -BlueCatSession $BlueCatSession
+        }
+
+        if ($Name) {
+            # Convert the Name into an Entity Object to use the object logic
+
+            if ($View) {
+                if (-not $View.ID) {
+                    $FailureMessage = "$($thisFN): Invalid View Object"
+                    Write-Verbose $FailureMessage
+                    throw $FailureMessage
+                }
+
+                if ($View.type -ne 'View') {
+                    $FailureMessage = "$($thisFN): Not a View - $($View.Name) is type '$($View.type)'"
+                    Write-Verbose $FailureMessage
+                    throw $FailureMessage
+                }
+            } else {
+                $View = Get-BlueCatView -ViewID $ViewID -BlueCatSession $BlueCatSession
+            }
+
+            $Object = Get-BlueCatAlias -Name $Name -View $View -BlueCatSession $BlueCatSession
+        }
+
+        if (-not $Object) {
+            if ($ID) {
+                $FailureMessage = "$($thisFN): Failed to convert Entity ID #$($ID) to an Alias Record"
+            } else {
+                $FailureMessage = "$($thisFN): Failed to convert Name '$($Name)' in View '$($View.name)' to an Alias Record"
+            }
+            Write-Verbose $FailureMessage
+            throw $FailureMessage
+        }
+
+        if (-not $Object.ID) {
+            $FailureMessage = "$($thisFN): Invalid Alias Object"
+            Write-Verbose $FailureMessage
+            throw $FailureMessage
+        }
+
+        if ($Object.type -ne 'AliasRecord') {
+            $FailureMessage = "$($thisFN): Not an Alias Record - $($Object.Name) is type '$($Object.type)'"
+            Write-Verbose $FailureMessage
+            throw $FailureMessage
+        }
+
+        $DeleteAlias = @{
+            ID             = $Object.ID
+            BlueCatSession = $BlueCatSession
+        }
+
+        Write-Verbose "$($thisFN): Deleting alias record for '$($Object.Name)' (ID:$($Object.ID))"
+        Remove-BlueCatEntityById @DeleteAlias
+    }
+}
