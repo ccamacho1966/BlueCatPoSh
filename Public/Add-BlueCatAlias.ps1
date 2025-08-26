c3498d59947b5c57247f4e15eb2da83e1ba874bf
Christopher <99899595+ccamacho-incomm@users.noreply.github.com>
2025-08-26 18:14:47 -0400

    Support Zone object for API efficiency

--- Public/Add-BlueCatAlias.ps1
+++ Public/Add-BlueCatAlias.ps1
@@ -73,6 +73,10 @@
         [Parameter()]
         [int] $TTL = -1,
 
+        [Parameter(ParameterSetName='ZoneObj',Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [PSCustomObject] $Zone,
+
         [Parameter(ParameterSetName='ViewID')]
         [ValidateRange(1, [int]::MaxValue)]
         [int] $ViewID,
@@ -96,80 +100,114 @@
     process {
         $thisFN = (Get-PSCallStack)[0].Command
 
+        if ($Zone) {
+            $View = $Zone.view
+        } elseif ($ViewID) {
+            $View = Get-BlueCatView -ViewID $ViewID -BlueCatSession $BlueCatSession
+        } elseif (-not $View) {
+            # No View or ViewID has been passed in so attempt to use the default view
+            $BlueCatSession | Confirm-Settings -View
+            Write-Verbose "$($thisFN): Using default view '$($BlueCatSession.View.name)' (ID:$($BlueCatSession.View.id))"
+            $View = $BlueCatSession.View
+        }
+
+        if (-not $View) {
+            throw "$($thisFN): View could not be resolved"
+        }
+
+        if (-not $View.ID) {
+            # This is not a valid object!
+            throw "$($thisFN): Invalid View object passed to function!"
+        }
+
+        if ($View.type -ne 'View') {
+            throw "$($thisFN): Object is not a View (ID:$($View.ID) $($View.name) is a $($View.type))"
+        }
+
+        # Trim any trailing dots from the name for consistency/display purposes
         $FQDN = $Name | Test-ValidFQDN
-        $LookupParms = @{
+
+        # Resolve zone if not provided
+        if (-not $Zone) {
+            $Zone = Resolve-BlueCatZone -Name $FQDN -View $View -BlueCatSession $BlueCatSession
+            if (-not $Zone) {
+                # Zone could not be resolved
+                throw "$($thisFN): Zone could not be resolved for $($FQDN)"
+            }
+        }
+
+        # Resolve short name for lookup
+        if ($FQDN -eq $Zone.name) {
+            $ShortName = ''
+        } else {
+            $ShortName = $FQDN -replace "\.$($Zone.name)$", ''
+        }
+
+        $LookupBase = @{
             Name           = $FQDN
             BlueCatSession = $BlueCatSession
+            ErrorAction    = 'SilentlyContinue'
         }
-        if ($ViewID) {
-            $LookupParms.ViewID = $ViewID
-        } elseif ($View)   {
-            $LookupParms.View   = $View
-            $ViewID             = $View.ID
-        }
+        $LookupZone = $LookupBase + @{ Zone = $Zone; SkipExternalHostCheck = $true }
+        $LookupView = $LookupBase + @{ View = $View }
 
-        $AliasInfo = Resolve-BlueCatFQDN @LookupParms
-        if (-not $AliasInfo.zone) {
-            # No deployable zone was found for Alias/CName
-            throw "No deployable zone was found for $($FQDN)"
-        }
-        Write-Verbose "$($thisFN): Selected Zone #$($AliasInfo.zone.id) as '$($AliasInfo.zone.name)'"
-
-        if ($AliasInfo.alias) {
+        if (Get-BlueCatAlias @LookupZone) {
             # There is already an existing alias
-            throw "Existing alias record found - aborting Alias creation!"
-        }
-
-        if ($AliasInfo.host) {
+            throw "$($thisFN): Existing alias record found - aborting Alias creation!"
+        } elseif (Get-BlueCatHost @LookupZone) {
             # There is already a host entry for this Alias/CName!!
-            throw "Existing host record found - aborting Alias creation!"
+            throw "$($thisFN): Existing host record found - aborting Alias creation!"
         }
 
-        if ($AliasInfo.external) {
-            Write-Warning "$($thisFN): An external host entry exists for '$($AliasInfo.external.name)'"
+        $ExistingExternal = Get-BlueCatExternalHost @LookupView
+        if ($ExistingExternal) {
+            Write-Warning "$($thisFN): An external host entry exists for '$($ExistingExternal.name)' (ID:$($ExistingExternal.id))"
         }
 
-        $LookupLinked      = $LookupParms
-        $NewLinked         = $LinkedHost | Test-ValidFQDN
-        $LookupLinked.Name = $NewLinked
+        $LookupLinked      = $LookupView
+        $LinkedFQDN        = $LinkedHost | Test-ValidFQDN
+        $LookupLinked.Name = $LinkedFQDN
 
         $LinkedInfo        = Resolve-BlueCatFQDN @LookupLinked
-        $propString = "ttl=$($TTL)|absoluteName=$($AliasInfo.name)|linkedRecordName=$($LinkedInfo.name)|"
+        $propString = "ttl=$($TTL)|absoluteName=$($FQDN)|linkedRecordName=$($LinkedInfo.name)|"
         if ($LinkedInfo.host) {
-            $linkedName = $LinkedInfo.host.name
-            Write-Verbose "$($thisFN): Found host record for linked host '$($linkedName)' (ID:$($LinkedInfo.host.id))"
+            $LinkedFQDN = $LinkedInfo.host.name
+            Write-Verbose "$($thisFN): Found host record for linked host '$($LinkedFQDN)' (ID:$($LinkedInfo.host.id))"
             if ($LinkedInfo.external) {
-                Write-Warning "$($thisFN): Both internal and external host entries found for $($linkedName)"
+                Write-Warning "$($thisFN): Both internal and external host entries found for $($LinkedFQDN)"
             }
             $propString += "linkedParentZoneName=$($LinkedInfo.zone.name)|"
         } elseif ($LinkedInfo.external) {
-            $linkedName = $LinkedInfo.external.name
-            Write-Verbose "$($thisFN): Found EXTERNAL host record for linked host '$($linkedName)' (ID:$($LinkedInfo.external.id))"
+            $LinkedFQDN = $LinkedInfo.external.name
+            Write-Verbose "$($thisFN): Found EXTERNAL host record for linked host '$($LinkedFQDN)' (ID:$($LinkedInfo.external.id))"
         } else {
-            throw "Aborting CNAME record creation: No host record found for linked host $($NewLinked)"
+            throw "Aborting CNAME record creation: No host record found for linked host $($LinkedFQDN)"
         }
 
         $Body = @{
             type       = 'AliasRecord'
-            name       = $AliasInfo.shortName
+            name       = $ShortName
             properties = $propString
         }
         $CreateAliasRecord = @{
             Method         = 'Post'
-            Request        = "addEntity?parentId=$($AliasInfo.zone.id)"
+            Request        = "addEntity?parentId=$($Zone.id)"
             Body           = ($Body | ConvertTo-Json)
             BlueCatSession = $BlueCatSession
         }
 
         $BlueCatReply = Invoke-BlueCatApi @CreateAliasRecord
-        if (-not $BlueCatReply) {
-            throw "CNAME record creation failed for $($FQDN)"
-        }
 
-        Write-Verbose "$($thisFN): Created ID:$($BlueCatReply) as '$($AliasInfo.name)' (points to '$($linkedName)')"
+        if ($BlueCatReply) {
+            Write-Verbose "$($thisFN): Created ID:$($BlueCatReply) as '$($FQDN)' (points to '$($LinkedFQDN)')"
 
-        if ($PassThru) {
-            Get-BlueCatEntityById -ID $BlueCatReply -BlueCatSession $BlueCatSession
+            if ($PassThru) {
+                Get-BlueCatAlias @LookupZone
+            }
+        } else {
+            $Failure = "$($thisFN): CNAME record creation failed for $($FQDN)"
+            throw $Failure
+            Write-Verbose $Failure
         }
     }
 }
