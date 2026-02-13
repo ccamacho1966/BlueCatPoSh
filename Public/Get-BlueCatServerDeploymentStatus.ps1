d83837cddaf495efb8636f3efb6f912c501b494a
Christopher <99899595+ccamacho-incomm@users.noreply.github.com>
2026-02-12 19:01:47 -0500

    Create Get-BlueCatServerDeploymentStatus.ps1

---
+++ Public/Get-BlueCatServerDeploymentStatus.ps1
@@ -0,0 +1,54 @@
+function Get-BlueCatServerDeploymentStatus {
+        [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [PSCustomObject] $Server,
+
+        [Parameter()]
+        [Alias('Connection','Session')]
+        [BlueCat] $BlueCatSession = $Script:BlueCatSession
+    )
+
+    begin {
+        Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+        if (-not $BlueCatSession) { throw 'No active BlueCatSession found' }
+    }
+
+    process {
+        $thisFN = (Get-PSCallStack)[0].Command
+
+        $ResultCodes = @{
+            '-1' = 'Deploying'
+             '0' = 'Initializing'
+             '1' = 'Queued'
+             '2' = 'Cancelled'
+             '3' = 'Failed'
+             '4' = 'Not Deployed'
+             '5' = 'Warning'
+             '6' = 'Invalid'
+             '7' = 'Success'
+             '8' = 'No Recent Deployment'
+        }
+
+        if ($Server.type -ne 'Server') {
+            throw "$($thisFN): Object is not a Server (ID:$($Server.ID) $($Server.name) is a $($Server.type))"
+        }
+
+        $LookupParms = @{
+            Method         = 'Get'
+            Request        = "getServerDeploymentStatus?serverId=$($Server.id)"
+            BlueCatSession = $BlueCatSession
+        }
+
+        $BlueCatReply = Invoke-BlueCatApi @LookupParms
+
+        $Status = @{
+            Code        = $BlueCatReply
+            Description = ($ResultCodes["$($BlueCatReply)"])
+        }
+
+        $Status
+    }
+}
