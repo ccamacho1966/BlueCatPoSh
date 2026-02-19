a69c2eba82225252f3fd2f8bd17669b73959f8bf
Christopher <99899595+ccamacho-incomm@users.noreply.github.com>
2026-02-19 16:31:32 -0500

    Add comment-based help text

--- Public/Update-BlueCatSOA.ps1
+++ Public/Update-BlueCatSOA.ps1
@@ -1,4 +1,56 @@
 function Update-BlueCatSOA {
+<#
+.SYNOPSIS
+    Update an existing Start of Authority record.
+.DESCRIPTION
+    The Update-BlueCatSOA cmdlet will update an existing Start of Authority record by allowing you to update specific fields.
+
+    Any fields not supplied as a parameter to this cmdlet will not be modified.
+.PARAMETER Name
+    A string value representing the FQDN of the DNS Zone that will have its Start of Authority record updated.
+.PARAMETER SOA
+    A PSCustomObject representing the existing Start of Authority record.
+.PARAMETER Zone
+    A PSCustomObject representing the DNS Zone that will have its Start of Authority record updated.
+.PARAMETER Email
+    A MailAddress object representing the email address of the published domain administrator.
+.PARAMETER OriginServer
+    A string value representing the FQDN of the published primary name server.
+.PARAMETER Refresh
+    Time in seconds that secondary servers wait before asking for the serial number again.
+.PARAMETER Retry
+    Time in seconds a secondary waits to retry a failed request.
+.PARAMETER Expire
+    Maximum time in seconds a secondary server can wait before treating its data as invalid.
+.PARAMETER Minimum
+    The default time-to-live for caching negative (not found) records.
+.PARAMETER ViewID
+    An integer value representing the entity ID of the desired view.
+.PARAMETER View
+    A PSCustomObject representing the desired view.
+.PARAMETER BlueCatSession
+    A BlueCat object representing the session to be used for this object creation.
+.PARAMETER PassThru
+    A switch that causes a PSCustomObject representing the new DNS Zone to be returned.
+.EXAMPLE
+    PS> Update-BlueCatSOA -Name 'example.com' -Refresh 600
+
+    Update the Start of Authority for example.com to have a refresh timer of 600 seconds (10 minutes).
+    BlueCatSession will default to the current default session.
+    View will default to the BlueCatSession default view.
+.EXAMPLE
+    PS> $NewSOA = ($MySOA | Update-BlueCatSOA -Email 'dnsadmin@example.com' -PassThru)
+
+    Update the Start of Authority record represented by $MySOA by updating the administrative email address.
+
+    A PSCustomObject representing the new Start of Authority will be returned (PassThru) and stored in the $NewSOA variable.
+.INPUTS
+    A PSCustomObject representing the existing Start of Authority record.
+.OUTPUTS
+    None, by default.
+
+    If the '-PassThru' switch is used, a PSCustomObject representing the updated Start of Authority record will be returned.
+#>
     [CmdletBinding(DefaultParameterSetName='ViewID')]
 
     param(
